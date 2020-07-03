11a92c585d477d5b89eb8795a0ea9d7b371d1ff6
shanilpanara <sp2017@ic.ac.uk>
2020-07-03 11:40:48 +0100

    ignore vscode files

--- origamiUROP/hello.py
+++ origamiUROP/hello.py
@@ -1,5 +1,12 @@
+import numpy as np
+
 """
 This file is to say HELLO days of coding and bye bye days of twidling my thumbs.
 """
 variable1 = "Hello1"
 variable2 = "Hello2"
+variable = np.zeros
+print "hello"
+
+test = "test"
+
