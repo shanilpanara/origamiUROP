97b65b35e20a5816eb9a32fcfa4c43cfaa15699b
shanilpanara <sp2017@ic.ac.uk>
2020-07-07 18:11:18 +0100

    new Polygons file with Edge, Vertex and Polygon classes

--- polygons/polygons.py
+++ polygons/polygons.py
@@ -4,18 +4,20 @@ import numpy as np
 class Vertex:
     """
     A Vertex is a point in space that is connected to other vertices
+    
+    Defined simply by: x, y and z (in 3D)
     """
 
     def __init__(self, position: np.ndarray):
         self.position = position
 
-    def distance(self, vertex: Vertex) -> float:
-        return np.linalg.norm(self.position - vertex.position)
+    def distance(self, another_vertex: Vertex) -> float:
+        return np.linalg.norm(self.position - another_vertex.position)
 
 
 class Edge:
     """ 
-    An Edge connects two vertices and stores geometric information about this connection 
+    An Edge is a combination of two vertices and stores geometric information about this connection 
     """
 
     def __init__(self, vertex_1: Vertex, vertex_2: Vertex):
@@ -27,3 +29,15 @@ class Edge:
     @property
     def vector(self) -> np.ndarray:
         return self.vertices[1] - self.vertices[0]
+
+
+class Polygon:
+    """
+    A Polygon is a cyclic combination of 3 or more edges
+
+    The first vertex on the starting edge = the last vertex on the final edge
+    """
+
+    def __init__(self, edges: np.ndarray):
+        self.edges = edges
+        self.n_sides = edges.shape[0]  # no of rows
