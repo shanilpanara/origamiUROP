0161b9487a482a0fcf949ebdf6be80ac3675bbb1
shanilpanara <sp2017@ic.ac.uk>
2020-07-07 16:01:04 +0100

    Display cube using verticies and faces

---
+++ origamiUROP/polygon 4.py
@@ -0,0 +1,61 @@
+import numpy as np
+import stl
+from stl import mesh
+
+# Define the 8 vertices of the cube
+vertices = np.array(
+    [
+        [-1, -1, -1],
+        [+1, -1, -1],
+        [+1, +1, -1],
+        [-1, +1, -1],
+        [-1, -1, +1],
+        [+1, -1, +1],
+        [+1, +1, +1],
+        [-1, +1, +1],
+    ]
+)
+# Define the 12 triangles composing the cube
+faces = np.array(
+    [
+        [0, 4, 1],
+        [1, 3, 2],
+        [0, 4, 7],
+        [0, 7, 3],
+        [4, 5, 6],
+        [4, 6, 7],
+        [5, 1, 2],
+        [5, 2, 6],
+        [2, 3, 6],
+        [3, 7, 6],
+        [0, 1, 5],
+        [0, 5, 4],
+    ]
+)
+
+# Create the mesh
+cube = mesh.Mesh(np.zeros(faces.shape[0], dtype=mesh.Mesh.dtype))
+for i, f in enumerate(faces):
+    for j in range(3):
+        cube.vectors[i][j] = vertices[f[j], :]
+
+cube.save("cube.stl", mode=stl.Mode.ASCII)
+
+# Optionally render the rotated cube faces
+from matplotlib import pyplot
+from mpl_toolkits import mplot3d
+
+# Create a new plot
+figure = pyplot.figure()
+axes = mplot3d.Axes3D(figure)
+
+# Render the cube
+axes.add_collection3d(mplot3d.art3d.Poly3DCollection(cube.vectors))
+
+
+# Auto scale to the mesh size
+scale = cube.points.flatten("A")
+axes.auto_scale_xyz(scale, scale, scale)
+
+# Show the plot to the screen
+pyplot.show()
