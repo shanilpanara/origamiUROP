b38725da73901a150717eef699a580256c517aaf
shanilpanara <sp2017@ic.ac.uk>
2020-07-09 17:55:10 +0100

    Added write_STL feature!

--- polygons/polygons.py
+++ polygons/polygons.py
@@ -1,4 +1,7 @@
 import numpy as np
+import meshio
+import matplotlib.pyplot as plt
+import matplotlib.tri as tri
 
 # class Vertex:
 #     """
@@ -75,37 +78,44 @@ class BoundaryPolygon:
     def __str__(self):
         return "This polygon has {} edges".format(self.n_edges)
 
+    def write_STL(self, filename: str):
+        triangulation = tri.Triangulation(self.vertices[:, 0], self.vertices[:, 1])
+        triangles = triangulation.get_masked_triangles()
+        cells = [("triangle", triangles)]
+        filename = filename + ".stl"
+        print(filename)
+        meshio.write_points_cells(filename, self.vertices, cells)
+
 
 # ---plot---#
-import matplotlib.pyplot as plt
-import numpy as np
-import matplotlib.tri as tri
-
-
 def plot_the_vertices(vertices: np.ndarray):
     fig = plt.figure()
     nodes = vertices
     triangulation = tri.Triangulation(nodes[:, 0], nodes[:, 1])
+    hello = triangulation.get_masked_triangles()
     plt.triplot(triangulation, "-k")
+    plt.axes().set_aspect("equal", "datalim")
     plt.show()
+    return hello
 
 
 # ---run the script---#
 def make_polygon(polygon_vertices: np.ndarray):
     shape_object = BoundaryPolygon(polygon_vertices)
 
-    plot_the_vertices(polygon_vertices)
+    hello = plot_the_vertices(polygon_vertices)
 
     i = 1
     # Current Functionalities
     print(f"This shape has {shape_object.edges.__len__()} edges \n")
     print(
-        f"The coordinates of edge number {i} are: {str(shape_object.edges[i].vertices[0])} and {str(shape_object.edges[i].vertices[1])} \n"
+        f"The coordinates of edge number {i+1} are: {str(shape_object.edges[i].vertices[0])} and {str(shape_object.edges[i].vertices[1])} \n"
     )
     print(f"It has a size of {round(shape_object.edges[i].__len__(),4)}\n")
-    print(f"This edge is a type of {shape_object.edges[i].type} edge")
+    print(f"This edge is a type of {shape_object.edges[i].type} edge \n")
+    print(f"It's vector is {shape_object.edges[i].vector}\n")
     print(f"It's unit vector is {shape_object.edges[i].unit_vector}")
-    return shape_object
+    return shape_object, hello
 
 
 ###---stored arrays of some shapes---###
@@ -113,8 +123,10 @@ square = np.array([[0, 0, 0], [0.0, 1.0, 0.0], [1.0, 1.0, 0.0], [1.0, 0.0, 0.0]]
 trapezium = np.array([[0, 0, 0], [1.0, 10.0, 0.0], [11.0, 10.0, 0.0], [15.0, 0.0, 0.0]])
 
 no = 0.866025
-hexagon = np.array([[0,1,0],[no,0.5,0],[0,-1,0],])
+hexagon = np.array(
+    [[0, 1, 0], [no, 0.5, 0], [no, -0.5, 0], [0, -1, 0], [-no, -0.5, 0], [-no, 0.5, 0]]
+)
 
 
 ###---run the program---###
-SHAPE = make_polygon(trapezium)
+SHAPE, hello = make_polygon(hexagon)
