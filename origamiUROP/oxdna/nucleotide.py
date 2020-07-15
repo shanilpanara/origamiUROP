f9816df818b4f6b31761926220e357862572d5de
Debesh-XPS <debesh.mandal@hotmail.com>
2020-07-15 12:36:13 +0100

    added some initialisation to Nucleotide

--- origamiUROP/oxdna/nucleotide.py
+++ origamiUROP/oxdna/nucleotide.py
@@ -1,3 +1,16 @@
+import numpy as np
+
 class Nucleotide():
-    def __init__(self):
-        pass
+    def __init__(
+        self, 
+        cm_pos : np.ndarray,
+        a1 : np.ndarray, 
+        a3 : np.ndarray,
+        v : np.ndarray = np.array([0., 0., 0.]),
+        L : np.ndarray = np.array([0., 0., 0.])
+    ):
+        self.cm_pos = cm_pos
+        self.a1 = a1
+        self.a3 = a3
+        self.v = v
+        self.L = L
