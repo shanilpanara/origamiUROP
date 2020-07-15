73cbb4f8204a475f4312d3a7f20f0839e6e286b8
Debesh-XPS <debesh.mandal@hotmail.com>
2020-07-15 12:51:56 +0100

    added init to System

--- origamiUROP/oxdna/system.py
+++ origamiUROP/oxdna/system.py
@@ -1,3 +1,18 @@
 class System():
-    def __init__(self):
-        pass
+    def __init__(
+        self, 
+        box : np.ndarray,
+        time : int = 0,
+        E_pot : float = 0.,
+        E_kin : float = 0.
+    ):
+
+    self.box = box
+    self.time = time
+    self.E_pot = E_pot
+    self.E_kin = E_kin
+
+    @property
+    def E_tot(self):
+        return self.E_pot + self.E_kin
+        
