1e364b2d7ec1046afbcb6d9c18189001817ff949
shanilpanara <sp2017@ic.ac.uk>
2020-07-17 16:30:52 +0100

    improved add_strand feature and corrected topology columns order for oxDNA output

--- origamiUROP/oxdna/system.py
+++ origamiUROP/oxdna/system.py
@@ -5,7 +5,7 @@ import pandas as pd
 from origamiUROP.oxdna import Strand, Nucleotide
 
 CONFIGURATION_COLUMNS = ["position", "a1", "a3", "v", "L"]
-TOPOLOGY_COLUMNS = ["base", "strand", "3p", "5p"]
+TOPOLOGY_COLUMNS = ["strand", "base", "3p", "5p"]
 
 
 def oxDNA_string(dataframe: pd.DataFrame) -> str:
@@ -32,10 +32,8 @@ def oxDNA_string(dataframe: pd.DataFrame) -> str:
     output = output.strip()
     output = output.replace("\n ", "\n")
     output += "\n"
-    # not sure why this replace statement is needed twice but
-    # it doesn't work otherwise
-    output = output.replace("  ", " ")
-    return output.replace("  ", " ")
+    # there are a combination of triple & double spaces, hence x2 .replace()
+    return output.replace("   ", " ").replace("  ", " ")
 
 
 class System:
@@ -75,7 +73,7 @@ class System:
         shift = 0
         for i, strand in enumerate(self._strands):
             strand._nucleotide_shift = shift
-            strand.index = i
+            strand.index = i + 1
             shift += len(strand)
         return self._strands
 
@@ -115,22 +113,34 @@ class System:
             f.write(oxDNA_string(self.topology))
 
     def add_strand(self, addition: Strand or list, index: int = None):
+        """
+        Method to add strand(s) to the system
+
+        Parameters:
+            addition: accepted as Strand objects or a List of Strands
+            index: will add given Strand(s) to the end if not given,
+            otherwise strands inserted at locations given
+                
+        """
+        # Determine whether to add to end or at a certain index (must be integer value)
         if index == None or index > len(self._strands):
             index = len(self._strands)  # add strands to end
         elif isinstance(index, int):
-            pass
+            pass  # index = index
         else:
             raise TypeError("Index must be an integer!")
 
         if not isinstance(addition, (Strand, list)):
             raise TypeError("Added Strands must be a Strand objects!")
-
         if isinstance(addition, Strand):
-            self._strands.insert(index, addition)
-
+            self._strands.insert(index, addition.copy())
         elif isinstance(addition[0], Strand):  # list items must be Strand objects
             for i in range(len(addition)):
-                self._strands.insert(index + i, addition[i])
+                self._strands.insert(index + i, addition[i].copy())
 
         else:
             raise TypeError("Added Strands must be a Strand objects!")
+
+        # Update all strand indicies
+        for i in range(len(self._strands)):
+            self._strands[i].index = i
