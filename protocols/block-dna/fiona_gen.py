d322fce2bc50996cdbb43f90ac1aaaf2be2188de
Debesh-XPS <debesh.mandal@hotmail.com>
2020-07-23 17:07:51 +0100

    updated generation and also corrected something in system

--- protocols/block-dna/fiona_gen.py
+++ protocols/block-dna/fiona_gen.py
@@ -1,10 +1,20 @@
-from origamiUROP.oxdna import System, Strand, Nucleotide
+#!/usr/bin/env python
+"""
+Script to generate an oxDNA system containing one piece of
+DNA that has single-strand and double-strand block co-
+polymerisation.
+"""
 from argparse import ArgumentParser
 
+import numpy as np
+
+from origamiUROP.oxdna import System, Strand, Nucleotide
+from origamiUROP.oxdna.strand import generate_helix, FENE_EPS
+
 def generate_system(
-        box : float,
         n : int,
-        fraction_ds : float
+        double_strand_length : int,
+        single_strand_length : int
     ) -> System:
     """
     Generates an oxDNA system containing a single piece of DNA
@@ -18,11 +28,53 @@ def generate_system(
     Returns:
         system - oxDNA system
     """
-    system = System()
+    # initialise system to comply with minimum image convention
+    box = 2. * n * FENE_EPS
+    print(f'Creating simulation system with box size: {box}')
+    system = System(np.array([box, box, box]))
+
+    # create a list of strands, our main strand which we will use
+    # and our complementary strand
+    strands = generate_helix(n, double=True)
+    
+    # make a copy of the complementary strand and remove it 
+    # from the main list of strands
+    second_strand = strands[1].copy()
+    strands = strands[:1]
+
+    # calculate how many portions there should be
+    portion_size = double_strand_length + single_strand_length
+    portions = n // portion_size
+    print(f'Creating {portions} portions of total size: {portion_size}')
+
+    # iterate over all portions, adding a new complementary
+    # strand to the list of strands we will use
+    for i in range(portions):
+        # take the nucleotides from each portion and
+        # create a new strand for each double-stranded part
+        # of the portion
+        start = portion_size * i
+        end = start + double_strand_length
+        nucleotides = second_strand.nucleotides[start:end]
+        new_strand = Strand(nucleotides=nucleotides)
+        strands.append(new_strand)
+
+    # finally add the strands to the system and return
+    # the system
+    system.add_strands(strands)
     return system
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument('-b', '--box', type=float)
-    parser.add_argument('-n', '--number', type=int)
-    parser.add_argument('-ds', '--double-stranded', type=float)
+    parser.add_argument('-n', '--number', type=int, required=True)
+    parser.add_argument('-ds', '--double-stranded', type=int, required=True)
+    parser.add_argument('-ss', '--single-stranded', type=int, required=True)
+    parser.add_argument('-f', '--output-prefix')
+    args = parser.parse_args()
+    system = generate_system(
+        args.number,
+        args.double_stranded,
+        args.single_stranded
+    )
+    if args.output_prefix:
+        system.write_oxDNA(args.output_prefix)
