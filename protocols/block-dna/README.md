49147f877354603dd5701ceefb4f5186d970ceab
Debesh-XPS <debesh.mandal@hotmail.com>
2020-07-27 13:58:50 +0100

    added box option

--- protocols/block-dna/README.md
+++ protocols/block-dna/README.md
@@ -3,10 +3,10 @@
 This folder contains code to generate block copolymer ssDNA/dsDNA.
 
 ## Usage
-To create a system containing a strand of 30bp with double-stranded parts of length 10bp and single-stranded parts of length 5bp, written to the files `oxdna.out.conf` and `oxdna.out.top`, run the following command:
+To create a system containing a strand of 30bp with double-stranded parts of length 10bp and single-stranded parts of length 5bp, written to the files `oxdna.out.conf` and `oxdna.out.top`, in a box of size 50.0, run the following command:
 
 ```
-python fiona_gen.py -n 30 -ds 10 -ss 5 -f out
+python fiona_gen.py -n 30 -ds 10 -ss 5 -f out -b 50
 ```
 
 Using Ovito to view, we can visualise a snapshot of this starting configuration:
--- protocols/block-dna/fiona_gen.py
+++ protocols/block-dna/fiona_gen.py
@@ -14,7 +14,8 @@ from origamiUROP.oxdna.strand import generate_helix, FENE_EPS
 def generate_system(
         n : int,
         double_strand_length : int,
-        single_strand_length : int
+        single_strand_length : int,
+        box : float = None,
     ) -> System:
     """
     Generates an oxDNA system containing a single piece of DNA
@@ -29,7 +30,8 @@ def generate_system(
         system - oxDNA system
     """
     # initialise system to comply with minimum image convention
-    box = 2. * n * FENE_EPS
+    if not box:
+        box = 2. * n * FENE_EPS
     print(f'Creating simulation system with box size: {box}')
     system = System(np.array([box, box, box]))
 
@@ -69,12 +71,15 @@ if __name__ == '__main__':
     parser.add_argument('-n', '--number', type=int, required=True)
     parser.add_argument('-ds', '--double-stranded', type=int, required=True)
     parser.add_argument('-ss', '--single-stranded', type=int, required=True)
+    parser.add_argument('-b', '--box', type=float, default=None)
     parser.add_argument('-f', '--output-prefix')
+    
     args = parser.parse_args()
     system = generate_system(
         args.number,
         args.double_stranded,
-        args.single_stranded
+        args.single_stranded,
+        box = args.box
     )
     if args.output_prefix:
         system.write_oxDNA(args.output_prefix)
