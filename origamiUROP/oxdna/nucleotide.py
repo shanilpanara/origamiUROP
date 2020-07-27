16e7b07925cb9e9dac8c367416c80319239b333a
Debesh-XPS <debesh.mandal@hotmail.com>
2020-07-27 15:18:51 +0100

    added quaternion property for nucleotide

--- origamiUROP/oxdna/nucleotide.py
+++ origamiUROP/oxdna/nucleotide.py
@@ -99,6 +99,48 @@ class Nucleotide:
         return np.cross(self._a3, self._a1)
 
     @property
+    def quaternion(self) -> np.ndarray:
+        """
+        Returns the quaternion used to create a LAMMPS configuration
+
+        Taken from https://github.com/lorenzo-rovigatti/tacoxDNA/src/libs/oxDNA_LAMMPS.py
+        """
+        result = [1., 0., 0., 0.]
+
+        a1, a2, a3 = (self._a1, self._a2, self._a3)
+
+        q0sq = 0.25 * (a1[0] + a2[1] + a3[2] + 1.0)
+        q1sq = q0sq - 0.5 * (a2[1] + a3[2])
+        q2sq = q0sq - 0.5 * (a1[0] + a3[2])
+        q3sq = q0sq - 0.5 * (a1[0] + a2[1])
+
+        if q0sq >= 0.25:
+            result[0] = np.sqrt(q0sq)
+            result[1] = (mya2[2] - mya3[1]) / (4.0 * result[0])
+            result[2] = (mya3[0] - mya1[2]) / (4.0 * result[0])
+            result[3] = (mya1[1] - mya2[0]) / (4.0 * result[0])
+        elif q1sq >= 0.25:
+            result[1] = np.sqrt(q1sq)
+            result[0] = (mya2[2] - mya3[1]) / (4.0 * result[1])
+            result[2] = (mya2[0] + mya1[1]) / (4.0 * result[1])
+            result[3] = (mya1[2] + mya3[0]) / (4.0 * result[1])
+        elif q2sq >= 0.25:
+            result[2] = np.sqrt(q2sq)
+            result[0] = (mya3[0] - mya1[2]) / (4.0 * result[2])
+            result[1] = (mya2[0] + mya1[1]) / (4.0 * result[2])
+            result[3] = (mya3[1] + mya2[2]) / (4.0 * result[2])
+        elif q3sq >= 0.25:
+            result[3] = np.sqrt(q3sq)
+            result[0] = (mya1[1] - mya2[0]) / (4.0 * result[3])
+            result[1] = (mya3[0] + mya1[2]) / (4.0 * result[3])
+            result[2] = (mya3[1] + mya2[2]) / (4.0 * result[3])
+
+        norm = 1.0 / np.sqrt(result[0] * result[0] + result[1] * result[1] + \
+                result[2] * result[2] + result[3] * result[3])
+
+        return np.array(result) * norm
+
+    @property
     def series(self) -> pd.Series:
         """
         Writes a pd.Series object containing the information
