26e4d454856692196df4dd991f729f92821c9548
Debesh-XPS <debesh.mandal@hotmail.com>
2020-07-30 16:28:24 +0100

    init LatticeRoute

---
+++ origamiUROP/lattice/route.py
@@ -0,0 +1,8 @@
+from ..tools import DNANode, DNAEdge
+from .edge import DNAEdge
+from .node import DNANode
+from ..oxdna import Strand
+
+class LatticeRoute(Strand):
+    def __init__(self):
+        return
