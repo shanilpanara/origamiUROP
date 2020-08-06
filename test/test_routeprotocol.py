5a046376eb4be6b725b2dc70936c29cfdc4ac51d
Debesh-XPS <debesh.mandal@hotmail.com>
2020-08-07 00:43:08 +0100

    put route.plot() in the __main__ section

--- test/test_routeprotocol.py
+++ test/test_routeprotocol.py
@@ -4,18 +4,21 @@ from origamiUROP.polygons import BoundaryPolygon
 
 import numpy as np
 
-
-def test_square():
+def square_route():
     square = np.array([[0, 0, 0], [0.0, 1.0, 0.0], [1.0, 1.0, 0.0], [1.0, 0.0, 0.0]])
     polygon = BoundaryPolygon(square * 10)
     lattice = polygon.lattice()
     nodes = lattice.route()
     route = LatticeRoute(nodes)
-    #route.plot()
+    return route
+
+def test_square():
+    route = square_route()
     system = System(np.array([50.0, 50.0, 50.0]))
     system.add_strand(route)
     system.write_oxDNA()
 
-
 if __name__ == "__main__":
+    route = square_route()
+    route.plot()
     test_square()
