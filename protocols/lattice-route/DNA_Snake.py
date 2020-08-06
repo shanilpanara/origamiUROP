374bd1d5c913d4c05445a15fd2f7233017da3928
shanilpanara <sp2017@ic.ac.uk>
2020-08-06 15:44:32 +0100

    Tweaked DNA_snake and added file root

--- protocols/lattice-route/DNA_Snake.py
+++ protocols/lattice-route/DNA_Snake.py
@@ -4,6 +4,10 @@ from origamiUROP.polygons import BoundaryPolygon
 
 import numpy as np
 
+from os import path
+
+ROOT = "/".join(path.abspath(__file__).split("/")[:-1])
+
 
 def generate(polygon_vertices: np.ndarray):
     polygon = BoundaryPolygon(polygon_vertices)
@@ -12,17 +16,16 @@ def generate(polygon_vertices: np.ndarray):
     nodes = lattice.route()
 
     route = LatticeRoute(nodes)
-
-    system = System(np.array([50.0, 50.0, 50.0]))
+    system = route.system()
     system.add_strand(route)
 
     route.plot()
-    system.write_oxDNA()
+    system.write_oxDNA(root=ROOT)
 
 
 def generate_square():
-    square = np.array([[0, 0, 0], [0.0, 1.0, 0.0], [1.0, 1.0, 0.0], [1.0, 0.0, 0.0]])
-    generate(square * 10)
+    square = np.array([[0, 0, 0], [10.0, 1.0, 0.0], [10.0, 1.0, 0.0], [1.0, 0.0, 0.0]])
+    generate(square)
 
 
 if __name__ == "__main__":
