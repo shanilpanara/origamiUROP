b0b6d517edb6438047fd2b9f9f83b6d95247b4e7
lamr18 <57528428+lamr18@users.noreply.github.com>
2020-08-21 15:45:38 +0100

    edit louise_gen fixed integer check

--- protocols/block-dna/louise_gen.py
+++ protocols/block-dna/louise_gen.py
@@ -9,13 +9,7 @@ from argparse import ArgumentParser
 import numpy as np
 
 from fiona_gen import generate_system
-#from origamiUROP.oxdna import System
-#import sys
-#sys.path.append('/Users/louiserosset/Documents/Github/origamiUROP/origamiUROP/oxdna')
-#from sys import System
-#from origamiUROP.oxdna import System
 from system import System
-sim_nb = 1
 
 if __name__ == '__main__':
     parser = ArgumentParser()
@@ -28,22 +22,43 @@ if __name__ == '__main__':
     tot_length = np.arange(args.number[0], args.number[2]+1, args.number[1])
     ds_length = np.arange(args.double_stranded[0], args.double_stranded[2]+1, args.double_stranded[1])
     ss_length = np.arange(args.single_stranded[0], args.single_stranded[2]+1, args.single_stranded[1])
+    
+    """with open("test", "w") as f:
+        for i in range (len(tot_length)):
+            f.write (str(tot_length[i]))
+        f.write ("end of tot_length")
+        
+        for i in range (len(ds_length)):
+            f.write (str(ds_length[i]))
+        f.write ("end of ds_length")
+        
+        for i in range (len(ss_length)):
+            f.write (str(ss_length[i]))
+        f.write ("end of ss_length")"""
 
-    sim_nb =0 
+    sim_nb = 0 
 
     for i in range (len(tot_length)):
         for j in range (len(ds_length)):
             for k in range (len(ss_length)):
-             
-             check = tot_length[i] / (ds_length[j] + ss_length[k])
-             if isinstance (check, int) == True:
-                 system = generate_system(
-                    tot_length[i],
-                    ds_length[j],
-                    ss_length[k]
-                )
-                 sim_nb +=1
 
-                 if args.output_prefix:
-                     system.write_oxDNA_folder(sim_nb, args.output_prefix)
-            
+                if tot_length[i]%(ds_length[j] + ss_length[k]) == 0:
+                    
+                    with open("test", "w") as f:
+                        for i in range (len(tot_length)):
+                            f.write (str(tot_length[i]))
+                        f.write ("end of tot_length")
+                        
+                        for i in range (len(ds_length)):
+                            f.write (str(ds_length[i]))
+                        f.write ("end of ds_length")
+                        
+                        for i in range (len(ss_length)):
+                            f.write (str(ss_length[i]))
+                        f.write ("end of ss_length")
+                    
+                    system = generate_system(tot_length[i], ds_length[j], ss_length[k])
+                    sim_nb +=1
+
+                    if args.output_prefix:
+                        system.write_oxDNA_folder(sim_nb, args.output_prefix)
