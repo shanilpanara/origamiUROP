eb37b98a9e53f55d42eaf776dcceefeb7adfb23a
Debesh-XPS <debesh.mandal@hotmail.com>
2020-10-06 13:33:57 +0100

    initial commit for lammps IO testing

---
+++ test/test_lammps/test_lammps_IO.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+def test_read_data():
+    return
+
+def test_read_dump():
+    return
+
+def test_write_data():
+    return
+
+if __name__=='__main__':
+    test_read_data()
+    test_read_dump()
+    test_write_data()
+
